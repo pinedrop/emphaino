23b61e056dddbb93dd933f35786ee3713410c034
Edward Garrett <edward@pinedrop.com>
2017-08-17 12:18:11 +0100

    put audio before video in list of post types

--- header.php
+++ header.php
@@ -74,7 +74,7 @@
 					</h1>
 				<?php endif; ?>
 				<header class="page-header" data-active-language="<?php print $language; ?>">
-					<?php echo do_shortcode( '[searchandfilter submit_label="Show" class="language-content-filter" all_items_labels="All languages,All" fields="language,post_types" types="select,radio" post_types="video,audio,person,image,resource"]'); ?>
+					<?php echo do_shortcode( '[searchandfilter submit_label="Show" class="language-content-filter" all_items_labels="All languages,All" fields="language,post_types" types="select,radio" post_types="audio,video,person,image,resource"]'); ?>
 				</header>
 						
 	<div id="main" class="site-main">
