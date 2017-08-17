db423b19b6ba8ea82548a036b03fe108ce3b6ce8
Edward Garrett <edward@pinedrop.com>
2017-08-17 16:42:44 +0100

    change CSS for hiding all post type selector

--- js/scripts.js
+++ js/scripts.js
@@ -64,6 +64,7 @@ jQuery(document).ready(function(){
     /* remove 'All languages' option from individual language pages */
     jQuery('header[data-active-language!="none"][data-active-language!="all"] select > option:first-child').remove();
 
+//header[data-active-language='none'] div > ul > li:nth-child(2) ul
     /* select 'Audio' radio button on home page to prevent showing all resource types for all languages */
     jQuery('header[data-active-language="none"] input[value=audio]').prop('checked', true);
 });
